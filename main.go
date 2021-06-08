c291c80df48c19d523b10c57e98121045b3838f5
0ni0nrings <strike3test@hotmail.com>
2021-06-08 12:53:03 +1200

    struct within struct

--- main.go
+++ main.go
@@ -2,16 +2,27 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
 type person struct {
 	firstName string
 	lastName  string
+	contact   contactInfo //contact filed assigned contactInfo struct
 }
 
 func main() {
-	var alex person
-	alex.firstName = "Alex"
-	alex.lastName = "Anderson"
-	fmt.Println(alex)
+	alex := person{
+		firstName: "alex",
+		lastName:  "anderson",
+		contact: contactInfo{
+			email:   "alex.anderson@gmail.com",
+			zipCode: 94000,
+		},
+	}
+
 	fmt.Printf("%+v\n", alex)
 
 }
